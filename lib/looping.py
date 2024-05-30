7cb9e9c59df8a8517552e2eda23789a916885b49
Gyzelle <gyznjeri@gmail.com>
2024-05-30 07:10:49 +0300

    python-p3-loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,26 @@
 #!/usr/bin/env python3
 
+# Function to countdown and print Happy New Year
 def happy_new_year():
-    # code goes here!
-    pass
+    countdown = 10
+    while countdown in range(10, 0, -1):  # Loop from 10 to 0 in reverse order
+        print(countdown)
+        countdown -= 1
+    
+    print("Happy New Year!")
 
+# Function to square all integers in a list
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return [i ** 2 for i in int_list]  # List comprehension to square each integer in the input list
 
+# Function to print FizzBuzz numbers
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):  # Loop from 1 to 100
+        if not i % 15:  # Check if the number is divisible by 15
+            print("FizzBuzz")
+        elif not i % 5:  # Check if the number is divisible by 5
+            print("Buzz")
+        elif not i % 3:  # Check if the number is divisible by 3
+            print("Fizz")
+        else:  # If none of the above conditions are met, print the number itself
+            print(i)
